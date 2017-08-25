bb396ac6efa458c0e8cc847b2e51136ab9cf46ea
chaninkou <michaelkoulove@yahoo.com>
2017-08-24 22:48:32 -0700

    testing if test branch work because of error

--- Example2.java
+++ Example2.java
@@ -5,8 +5,9 @@ public class Example2 {
 	public static void main(String[] args) {
 		int x = 1;
 		int y = 2;
+		double z = 2.13;
 		System.out.println(x + " + " + y + " = " + (x+y));
-		
+		System.out.println("Use double for number with decimal " + z);
 	}
 
 }
