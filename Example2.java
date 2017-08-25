a63874d5118c2fd5a839e075c11d5ca718951dca
chaninkou <michaelkoulove@yahoo.com>
2017-08-24 23:07:25 -0700

    Lets see if I messed up on changing default

--- Example2.java
+++ Example2.java
@@ -5,7 +5,7 @@ public class Example2 {
 	public static void main(String[] args) {
 		int x = 1;
 		int y = 2;
-		double z = 2.13;
+		double z = 2.14;
 		System.out.println(x + " + " + y + " = " + (x+y));
 		System.out.println("Use double for number with decimal " + z);
 	}
