0eb14fc08cb484a47417095c6232288239f574c1
chaninkou <michaelkoulove@yahoo.com>
2020-01-13 13:23:06 -0800

    Great review on 2D Matrix, hard to find patterns but easy to understand.

--- src/matrix2D/RotateMatrix90degreeFunction.java
+++ src/matrix2D/RotateMatrix90degreeFunction.java
@@ -11,16 +11,22 @@ public class RotateMatrix90degreeFunction {
 			for(int j = i; j< matrix.length; j++){
 				// Temp to keep track of matrix that is getting replaced
 				temp = matrix[i][j];
+				
+				// [0][1] = [1][0]
+				// [0][2] = [2][0]
 				matrix[i][j] = matrix[j][i];
 				matrix[j][i] = temp;
 			}
 		}
 		
-		// flip the matrix horizontally for each row
+		// flip the matrix from start and end for each row
 		for(int i = 0; i < matrix.length; i++){
 			for(int j = 0; j < matrix.length/2; j++){ // matrix.length / 2 is the amount of number getting flip
 				secondTemp = matrix[i][j];
+				
+				// matrix.length - 1 - j will get the last element and then second last and so on
 				matrix[i][j] = matrix[i][matrix.length - 1 - j];
+				
 				matrix[i][matrix.length - 1 - j] = secondTemp;
 			}
 		}
